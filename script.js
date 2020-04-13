38707ec543806887c94fb97ac4972e69b33316b4
javaminions <63418158+javaminions@users.noreply.github.com>
2020-04-13 13:48:33 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $("#Orders").click(function(){
-    $("#externalPlaceholder").load("Orders.html");
-  });
-  
-  $("#WishList").click(function(){
-    $("#externalPlaceholder").load("WishList.html");
-  });
-  
-  $("#Profile").click(function(){
-    $("#externalPlaceholder").load("Profile.html");
-  });
-  
-  $("#Logout").click(function(){
-    $("#externalPlaceholder").html("<h2>You have been logged out successfully. Thank you and enjoy your day</h2>");
-  });
+					$("#Orders").click(function() {
+						$("#externalPlaceholder").load("Orders.html");
+					});
 
-});
+					$("#WishList").click(function() {
+						$("#externalPlaceholder").load("WishList.html");
+					});
+
+					$("#Profile").click(function() {
+						$("#externalPlaceholder").load("Profile.html");
+					});
+					
+					$("#Cart").click(function() {
+						$("#externalPlaceholder").load("Cart.html");
+					});
+
+					$("#Logout").click(function() {
+						$("#externalPlaceholder").html("<h2 style=\"margin: auto; width: 60%;\"><br><br>You have been logged out successfully.<br><br>Thank you and enjoy your day</h2>");
+					});
+					
+					$('#ContactUs').click(function() {
+						$("#externalPlaceholder").load("ContactUs.html");
+					});
+
+					$(".dropdown").hover(function() {
+						$('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideDown("400");
+						$(this).toggleClass('open');
+						},function() {
+							$('.dropdown-menu', this).not(
+										'.in .dropdown-menu').stop(true, true)
+										.slideUp("400");
+								$(this).toggleClass('open');
+							});
+
+				});
